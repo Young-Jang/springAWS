0d987f58693943a95d58bd6f9db141e0e56d99ed
Young-Jang <b116104@naver.com>
2021-04-18 23:02:10 +0900

    connect to android

--- Adverse/src/main/java/com/example/demo/controller/UserProfileController.java
+++ Adverse/src/main/java/com/example/demo/controller/UserProfileController.java
@@ -1,15 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.mapper.UserProfileMapper;
@@ -48,4 +54,25 @@ public class UserProfileController {
 	public void deleteUserProfile(@PathVariable("id")String id) {
 		mapper.deleteUserProfile(id);
 	}
+	
+	@RequestMapping("/allUser")
+	@ResponseBody
+	    public List <Map<String,String>> androidTestWithRequestAndResponse(HttpServletRequest request){
+	        System.out.println(request.getParameter("title"));
+	        
+	        Map<String, String> result = new HashMap<String, String>();
+	        List <Map<String,String>> resultlist = new ArrayList<Map<String,String>>();
+	        List<UserProfile> tempList = mapper.getUserProfileList();
+	        for(UserProfile user : tempList)
+	        {
+		        result.put("id", user.getId());
+		        result.put("address", user.getAddress());
+		        result.put("name", user.getName());
+		        result.put("phone", user.getPhone());
+		        resultlist.add(result);
+	        }
+	        
+	        return resultlist;
+	    }
+
 }
